06ff806bf4243483a2678904507b329372cbcffb
Steven Gates <gates.steven@gmail.com>
2023-05-21 17:12:00 -0500

    Part 1

---
+++ Board.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        // Define players
+        public Player Player_1 { get; set; }
+        public Player Player_2 { get; set; }
+        public string Empty_Square { get; set; } = ".";
+        public Player CurrentPlayer { get; set; }
+        // Define board position
+        public Dictionary<Tuple<int, int>, string> Position { get; set; }
+
+        // Default constructor
+        public Board()
+        {
+            Player_1 = new Player { Name = "X" };
+            Player_2 = new Player { Name = "O" };
+            CurrentPlayer = Player_1;
+            Position = new Dictionary<Tuple<int, int>, string>();
+
+
+            for (int row = 0; row <= 3; row++)
+            {
+                for (int col = 0; col <= 3; col++)
+                {
+                    Position[new Tuple<int, int>(row, col)] = Empty_Square;
+                }
+            }
+        }
+
+        // Copy constructor
+        public Board(Board board)
+        {
+            Player_1 = board.Player_1;
+            Player_2 = board.Player_2;
+            Position = board.Position;
+        }
+
+        public void Print(Board board)
+        {
+            string boardString = String.Empty;
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    string key = $"({row},{col})";
+                    if (board.Position.TryGetValue(new Tuple<int, int>(row, col), out string value))
+                    {
+                        //Console.Write($"{key}: {value} ");
+                        boardString += value;
+                    }
+                }
+                boardString += "\n";
+            }
+
+            if(board.CurrentPlayer == Player_1)
+            {
+                boardString = $"\n ---------- \n '{Player_1.Name}' Turn: \n ---------- \n" + boardString;
+            }
+            else
+            {
+                boardString = $"\n ---------- \n '{Player_2.Name}' Turn: \n ---------- \n" + boardString;
+            }
+            Console.WriteLine($"{boardString}");
+        }
+
+        public Board Move(int row, int col)
+        {
+            // Create new board instance
+            Board board = new Board();
+
+            // Make move
+            board.Position[new Tuple<int, int>(row, col)] = Player_1.Name;
+
+            // Switch Players
+            board.CurrentPlayer = (CurrentPlayer == Player_1) ? Player_2 : Player_1;
+
+            // return new board state
+            return board;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,55 +5,21 @@ namespace TicTacToe
 {
     class Program
     {
-      
 
         static void Main(string[] args)
         {
-            board = new TicTacToeBoard();
-            mcts = new TreeSearch<Node>(board);
+            Board board = new Board();
+            //board.Position[new Tuple<int, int>(2, 2)] = "X";
             Console.WriteLine("Welcome to the game!");
-            while (!isGameOver)
-            {
-
-                if (board.CurrentPlayer == 1)
-                {
-                    Console.Clear();
-                    
-                    Console.WriteLine("-------------------");
-                    Console.WriteLine("");
-                    board.Draw();
-                    Console.WriteLine("");
-                    Console.WriteLine("Select a move: ");
-                    Console.WriteLine("1 = Top Left");
-                    Console.WriteLine("2 = Left Middle");
-                    Console.WriteLine("3 = Left Bottom");
-                    Console.WriteLine("4 = Top Middle");
-                    Console.WriteLine("5 = Center Piece");
-                    Console.WriteLine("6 = Bottom Middle");
-                    Console.WriteLine("7 = Top Right");
-                    Console.WriteLine("8 = Right Middle");
-                    Console.WriteLine("9 = Right Bottom");
-                    Console.WriteLine("");
-                    // Read user input
-                    int move = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("");
-                    board.PerformUserAction(move);
-                    board.Draw();
-                    Console.WriteLine("");
-                    // startingBoard.MakeMove(move)
-                    // mcts.Step();
-                }
-                else
-                {
-
-                   // mcts.Step();
-
-                    Node bestNode = mcts.BestNodeChoice();
-                    //board.
-                    board.Draw();
-                    Console.WriteLine("");
-                }
-            }
+            Console.WriteLine($"Current Player: {board.CurrentPlayer.Name}");
+            //Console.WriteLine($"Board: Player_X = {board.Player_X}, Player_O = {board.Player_O}, Position = {board.Position}");
+            //board.Print(board);
+            Board board1 = board.Move(0, 0);
+            board.Print(board1);
+            Console.WriteLine($"Current Player: {board1.CurrentPlayer.Name}");
+            board1.Print(board1);
+            Console.Read();
+ 
         }
     }
 }
