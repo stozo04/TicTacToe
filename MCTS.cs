cfc983e46ce0aa567f6481e186b642c69a5ffe7a
Steven Gates <gates.steven@gmail.com>
2023-05-23 16:19:58 -0500

    working correctly!

--- MCTS.cs
+++ MCTS.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
 
 namespace TicTacToe
 {
     public class MCTS
     {
-       // public TreeNode Root { get; set; }
 
-        public MCTS()
-        {
-            
-        }
+        public MCTS() { }
         
         // Search for the best move given a position
         public TreeNode Search(Board board)
@@ -44,15 +41,86 @@ namespace TicTacToe
         }
 
         // Select most promising node
-        public TreeNode Select(TreeNode root)
+        public TreeNode Select(TreeNode node)
         {
-            throw new NotImplementedException();
+            // Make sure that we are dealing with non-terminal nodes
+            while (!node.IsTerminal)
+            {
+                // Case where the node is fully expanded
+                if (node.IsFullyExpanded) {
+                    node = GetBestMove(node, 2);
+                }
+                else
+                {
+                    // Case where the node is not fully expanded
+                    return Expand(node);
+                }
+            }
+
+            return node;
         }
+
+        private TreeNode Expand(TreeNode node)
+        {
+            // Generate legal states (moves) for the given node
+            var states = node.Board.GenerateStates(node.Board);
+
+            // Loop over generated states (moves)
+            foreach (var state in states)
+            {
+                // Create a string key for every position in the state
+                foreach (var position in state.Position.Keys)
+                {
+                    // Convert the Tuple to a string key
+                    string key = $"{position.Item1},{position.Item2}";
+
+                    // Make sure that current state (move) is not present in child nodes
+                    if (!node.Children.ContainsKey(key))
+                    {
+                        // Create a new node
+                        TreeNode newNode = new TreeNode(state, node);
+
+                        // Add child node to parent's node chilren list
+                        node.Children.Add(key, newNode);
+
+                        // Figure out whether current (parent) node is fully expanded or not
+                        node.IsFullyExpanded = states.Count == node.Children.Count;
+
+                        // return newly created node
+                        return newNode;
+                    }
+                }
+            }
+
+            throw new Exception("Should never reach here");
+        }
+
 
         // Simulate the game via making random moves until reach end of game
         public int Rollout(Board board)
         {
-            throw new NotImplementedException();
+            // Make random moves for both sides untill terminal state of the game is reached
+            while (!board.HasWinner(board))
+            {
+                // Try to make a move
+                try
+                {
+                    // Make move on board
+                    List<Board> availableStates = board.GenerateStates(board);
+                    Random random = new Random();
+                    int randomMove = random.Next(availableStates.Count);
+                    board = availableStates[randomMove];
+                    board.Print(board);
+                }
+                catch(Exception e)
+                {
+                    // No moves available
+                    return 0; // Draw
+                }
+            }
+
+            // Return score from the "X" perspective
+            return board.CurrentPlayer.Name == board.Player_1.Name ? 1 : -1;            
         }
 
         // Backpropagate the number of visits and scoreup to the root node
@@ -75,11 +143,12 @@ namespace TicTacToe
                 }
 
                 // Get move score using UCT Formula
+                double moveScore = currentPlayer * child.Score / child.Visits + explorationConstant * Math.Sqrt(
+               2 * Math.Log(node.Visits) / child.Visits);
+                //double moveScore = (currentPlayer * child.Score / child.Visits)
+                //    + (explorationConstant * Math.Sqrt(2 * Math.Log(node.Score) / node.Visits));
+                Console.WriteLine($"MoveScore: {moveScore}");
 
-                double moveScore = (currentPlayer * child.Score / child.Visits)
-                    + (explorationConstant * Math.Sqrt(2 * Math.Log(node.Score) / node.Visits));
-                //double moveScore = (currentPlayer * node.Score / node.Visits)
-                //   + (explorationConstant * Math.Sqrt(2 * Math.Log(node.Score) / node.Visits));
                 // Better move has been found
                 if (moveScore > bestScore)
                 {
@@ -106,10 +175,10 @@ namespace TicTacToe
 
     public class TreeNode
     {
-        public Board Board { get; private set; }
-        public TreeNode ParentNode { get; private set; }
-        public bool IsTerminal { get; private set; }
-        public bool IsFullyExpanded { get; private set; }
+        public Board Board { get; set; }
+        public TreeNode ParentNode { get; set; }
+        public bool IsTerminal { get; set; }
+        public bool IsFullyExpanded { get; set; }
         public int Visits { get; set; }
         public int Score { get; set; }
         public Dictionary<string, TreeNode> Children { get; set; }
--- Program.cs
+++ Program.cs
@@ -82,46 +82,63 @@ namespace TicTacToe
         //}
 
         // TEST MCTS LOGIC
+        //static void Main(string[] args)
+        //{
+        //    // Create board instance
+        //    Board board = new Board();
+        //    TreeNode root = new TreeNode(board, null);
+        //    root.Visits = 6;
+        //    root.Score = 6;
+        //    // Generate States
+        //    var availableActions = board.GenerateStates(board);
+        //    // init move 1
+        //    var move1 = new TreeNode(availableActions[0], root);
+        //    move1.Visits = 2;
+        //    move1.Score = 2;
+
+        //    // init move 2
+        //    var move2 = new TreeNode(availableActions[1], root);
+        //    move2.Visits = 4;
+        //    move2.Score = 4;
+
+        //    // Create child nodes
+        //    root.Children = new Dictionary<string, TreeNode>()
+        //    {
+        //        { "key1", move1 },
+        //        { "key2", move2 },
+        //        // Add more key-value pairs as needed
+        //    };
+
+        //    // Create MCTS 
+        //    MCTS mcts = new MCTS();
+
+        //    // Call to get best move assuming search is finished (exploration constant = 0)
+        //    TreeNode bestMove = mcts.GetBestMove(root, 0);
+        //    board.Print(bestMove.Board);
+
+
+        //    // Call to get best move assuming search is finished (exploration constant = 0)
+        //    TreeNode bestMove2 = mcts.GetBestMove(root, 10);
+        //    board.Print(bestMove2.Board);
+        //    // WriteObjectProperties(bestMove);
+
+        //    Console.Read();
+        //}
+
         static void Main(string[] args)
         {
             // Create board instance
             Board board = new Board();
-            TreeNode root = new TreeNode(board, null);
-            root.Visits = 6;
-            root.Score = 6;
-            // Generate States
-            var availableActions = board.GenerateStates(board);
-            // init move 1
-            var move1 = new TreeNode(availableActions[0], root);
-            move1.Visits = 2;
-            move1.Score = 2;
-
-            // init move 2
-            var move2 = new TreeNode(availableActions[1], root);
-            move2.Visits = 4;
-            move2.Score = 4;
-
-            // Create child nodes
-            root.Children = new Dictionary<string, TreeNode>()
-            {
-                { "key1", move1 },
-                { "key2", move2 },
-                // Add more key-value pairs as needed
-            };
 
             // Create MCTS 
             MCTS mcts = new MCTS();
 
-            // Call to get best move assuming search is finished (exploration constant = 0)
-            TreeNode bestMove = mcts.GetBestMove(root, 0);
-
-
-            board.Print(bestMove.Board);
-
-            // WriteObjectProperties(bestMove);
+            // Simulate random game
+            mcts.Rollout(board);
 
             Console.Read();
         }
+
         static void WriteObjectProperties(object obj)
         {
             Type type = obj.GetType();
